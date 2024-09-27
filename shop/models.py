6121b3ecf5a8016bf5553cd7d9b853451cc32c81
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-27 22:44:38 +0900

    モデルの作成（仮）

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class item(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_image = models.ImageField()
+    item_price = models.IntegerField()
+    # item_rating
+    
