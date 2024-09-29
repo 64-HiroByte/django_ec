0237a8dcc916ba2606c6f879c9d1c8361ccefbed
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-29 11:42:13 +0900

    settings.pyにアプリを登録し、モデルにフィールドを追加した

--- shop/models.py
+++ shop/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 class item(models.Model):
-    item_name = models.CharField(max_length=100)
-    item_image = models.ImageField()
-    item_price = models.IntegerField()
-    # item_rating
-    
+    name = models.CharField(verbose_name='商品名', max_length=100)
+    price = models.IntegerField(verbose_name='価格')
+    description = models.TextField(verbose_name='説明')
+    created_at = models.DateTimeField(verbose_name='登録日', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='更新日', auto_now=True)
+    # rating は現状で設定しない
+
+    def __str__(self):
+        return self.name
