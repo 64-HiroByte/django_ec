efe0ff06530293dcf4253842aa48c710d12105c0
64-HiroByte <higher.ground.rc@gmail.com>
2024-10-05 18:28:02 +0900

    templatesディレクトリの移動とbase.html, item_detail.htmlの新規作成およびそれに伴うURL設定

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from django.views.generic import ListView
 
 from shop.models import Item
@@ -9,3 +10,11 @@ class ItemListView(ListView):
     model = Item
     template_name = "shop/item_list.html"
     context_object_name = 'items'
+
+
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "shop/item_detail.html"
+
