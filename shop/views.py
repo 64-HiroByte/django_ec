863ed7db43138e1b8997dc5dda94a6d4463662bf
64-HiroByte <higher.ground.rc@gmail.com>
2024-10-11 09:42:24 +0900

    商品詳細ページに関連商品として、登録日が最新のもの1点を表示するようにした

--- shop/views.py
+++ shop/views.py
@@ -16,3 +16,16 @@ class ItemListView(ListView):
 class ItemDetailView(DetailView):
     model = Item
     template_name = "shop/item_detail.html"
+    context_object_name = 'item'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # 現在のアイテムを取得
+        item = self.get_object()
+        # 表示させる関連商品の数
+        get_related_item_count = 1
+
+        # 関連商品の取得（ただし、現在の商品は除く）
+        context['related_items'] = Item.objects.exclude(id=item.id).order_by('-created_at')[:get_related_item_count]  # 登録日が最新のもの1点
+        return contex）
