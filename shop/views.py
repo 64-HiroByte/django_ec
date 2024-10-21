04d92a44ecd84c13e82ac00b40cdc3f4d0ef2612
64-HiroByte <higher.ground.rc@gmail.com>
2024-10-21 21:24:18 +0900

    item_list.html, item_detail.html, views.pyの修正と不要なディレクトリの削除

--- shop/views.py
+++ shop/views.py
@@ -24,8 +24,8 @@ class ItemDetailView(DetailView):
         # 現在のアイテムを取得
         item = self.get_object()
         # 表示させる関連商品の数
-        get_related_item_count = 1
+        get_related_item_count = 4
 
         # 関連商品の取得（ただし、現在の商品は除く）
-        context['related_items'] = Item.objects.exclude(id=item.id).order_by('-created_at')[:get_related_item_count]  # 登録日が最新のもの1点
+        context['related_items'] = Item.objects.exclude(id=item.id).order_by('-created_at')[:get_related_item_count] 
         return context
