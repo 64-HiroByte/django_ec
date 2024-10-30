d8933997572ef99892f2b3850c160fecb923ad4a
64-HiroByte <higher.ground.rc@gmail.com>
2024-10-30 22:07:29 +0900

    views.pyにclass ManagementListViewを定義した

--- item_management/views.py
+++ item_management/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from shop.models import Item
 # Create your views here.
+class ManagementListView(ListView):
+    model = Item
+    template_name = "admin/items.html"
+    context_object_name = 'items'
+    ordering = 'created_at'
