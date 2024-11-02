4c52ae87eaaedd14f1e12d8e4072feab6ff7febf
64-HiroByte <higher.ground.rc@gmail.com>
2024-11-02 17:23:57 +0900

    商品登録に関するルーティング、ビュー、htmlファイルを新規作成した

--- item_management/urls.py
+++ item_management/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from item_management.views import ItemCreateView
 from item_management.views import ManagementListView
 
 
 urlpatterns = [
     path('', ManagementListView.as_view(), name='management-item-list'),
+    path('register', ItemCreateView.as_view(), name='item-register'),
 ]
--- item_management/views.py
+++ item_management/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from django.views.generic import ListView
 
 from shop.models import Item
-# Create your views here.
+
+
 class ManagementListView(ListView):
     model = Item
     template_name = "item_management/management_list.html"
     context_object_name = 'items'
     ordering = 'created_at'
+
+
+
+class ItemCreateView(CreateView):
+    model = Item
+    template_name = "item_management/item_form.html"
+    fields = '__all__'
+    success_url = reverse_lazy('management-item-list')
