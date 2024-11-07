6441b99e304f107d7e9526bdc5f84196ae32ddf1
64-HiroByte <higher.ground.rc@gmail.com>
2024-11-07 11:37:15 +0900

    accountsにurl.pyを新規作成、ログイン用のビューを作成し、url周りの設定を行った。

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+from accounts.views import MyLoginView
+
+urlpatterns = [
+    path('login/', MyLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', TemplateView.as_view(template_name='hello.html')),
     path('', include('shop.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 # この設定が本当に必要なのか調べる 設定しなくても大丈夫な気がする（2024.10.20)
