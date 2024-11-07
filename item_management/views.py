03257c264239a1754d2e01157d0369b9f82b26dc
64-HiroByte <higher.ground.rc@gmail.com>
2024-11-07 17:26:29 +0900

    views.pyにLoginRequiredMixinを追加した。商品管理関係のページにナビバーを配置し、HTMLファイルを微修正した。

--- item_management/views.py
+++ item_management/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -8,7 +9,7 @@ from django.views.generic import UpdateView
 from shop.models import Item
 
 
-class ManagementListView(ListView):
+class ManagementListView(LoginRequiredMixin, ListView):
     model = Item
     template_name = "item_management/management_list.html"
     context_object_name = 'items'
@@ -16,7 +17,7 @@ class ManagementListView(ListView):
 
 
 
-class ItemCreateView(CreateView):
+class ItemCreateView(LoginRequiredMixin, CreateView):
     model = Item
     template_name = "item_management/item_form.html"
     fields = '__all__'
@@ -24,13 +25,13 @@ class ItemCreateView(CreateView):
 
 
 
-class ItemDeleteView(DeleteView):
+class ItemDeleteView(LoginRequiredMixin, DeleteView):
     model = Item
     template_name = "item_management/item_delete.html"
     success_url = reverse_lazy('management-item-list')
 
 
-class ItemUpdateView(UpdateView):
+class ItemUpdateView(LoginRequiredMixin, UpdateView):
     model = Item
     template_name = "item_management/item_form.html"
     fields = '__all__'
