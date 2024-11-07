ceb72b786f2490c3d1b636929e98448c7fe3d98f
64-HiroByte <higher.ground.rc@gmail.com>
2024-11-07 12:54:18 +0900

    login.htmlを新規作成し、リダイレクト等のURL周りの設定を行った。

--- config/settings/base.py
+++ config/settings/base.py
@@ -113,3 +113,7 @@ STORAGES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# login/logout redirect
+LOGOUT_REDIRECT_URL = 'item-list'
+LOGIN_REDIRECT_URL = 'item-list'
