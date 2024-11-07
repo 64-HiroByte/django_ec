3c630afc685a3a4f9fd877fcdeef245ad8824f28
64-HiroByte <higher.ground.rc@gmail.com>
2024-11-07 16:25:41 +0900

    トップ画面にログイン・ログアウトのボタンと商品管理ページへのリンクを追加した。

--- config/settings/base.py
+++ config/settings/base.py
@@ -114,6 +114,7 @@ STORAGES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# login/logout redirect
+# login/logout
+LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'item-list'
 LOGIN_REDIRECT_URL = 'item-list'
