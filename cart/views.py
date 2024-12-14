b59ba850916bd2272463e67b3bcfb5e8255e7bb0
64-HiroByte <higher.ground.rc@gmail.com>
2024-12-14 19:35:51 +0900

    カートの新規作成とカートに商品を追加するビューを定義した

--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,52 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
+from django.views.generic import CreateView
 from django.views.generic import TemplateView
+from django.views.generic import View
 
-from cart.cart import Cart
+# from cart.cart import Cart
+from cart.models import Cart
+from cart.models import CartItem
+from shop.models import Item
 
 
 CART_SESSION_KEY = 'cart'
 
+
+class CartCreateview(CreateView):
+    model = Cart
+    fields = []
+    template_name = ''  # のちほど決める
+
+    def form_valid(self, form):
+        cart = form.save()
+        self.request.session[CART_SESSION_KEY] = cart.pk
+        return redirect('')  # 商品をカートに追加するページへリダイレクト
+
+
+class AddToCartView(View):
+    def get_cart(self):
+        cart_pk = self.request.session.get(CART_SESSION_KEY)
+
+        if cart_pk is None:
+            cart = Cart.objects.create()
+            self.request.session[CART_SESSION_KEY] = cart.pk
+            return cart
+        else:
+            return Cart.objects.get(pk=cart_pk)
+    
+    def post(self, request, *args, **kwargs):
+        cart = self.get_cart()
+        item_pk = request.POST.get('item_pk')  # 商品一覧ページの数量もフォームを使う
+        item = get_object_or_404(Item, pk=item_pk)
+        
+        quantity = int(request.POST.get('quantity'))
+        CartItem.add_item(cart, item, quantity)
+        
+        return redirect('')  # リダイレクト先は後で決める
+
+
+"""
 class CheckoutListView(TemplateView):
     template_name = "cart/checkout.html"
     
@@ -30,4 +71,4 @@ class CheckoutListView(TemplateView):
             cart.save_to_session(request.session, CART_SESSION_KEY)
         
         return redirect('cart:checkout')
-
+"""
