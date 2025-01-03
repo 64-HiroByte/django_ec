119a6624835a3b9664caf50118f937c380a57381
64-HiroByte <higher.ground.rc@gmail.com>
2025-01-03 17:41:29 +0900

    動作確認のためにhtml、viewの内容の一部を簡略化した。

--- purchase/views.py
+++ purchase/views.py
@@ -14,40 +14,47 @@ from cart.models import CartItem
 class PurchaseView(View):
     
     def post(self, request, *args, **kwargs):
+        print('-' * 30 + ' post ' + '-'* 30)
         cart = Cart.load_from_session(request.session)
         if cart is None or cart.quantities == 0:
-            # カートがないまたは、数量が０の場合のredirect先を考える
-            return 
+            # カートがない、または、数量が０の場合、トップページへリダイレクト
+            print('カートが空です！')
+            return redirect('shop:item-list')
+        # 以下のelse文の内容は注文履歴の処理を行う際に有効化する
+        # else:
+        #     cart_items = CartItem.objects.select_related('item', 'cart').filter(cart_id=cart.pk)
         
         purchaser_form = PurchaserForm(request.POST)
-        shipping_address_form = ShippingAddressForm(request.POST)
-        credit_card_form = CreditCardForm(request.POST)
+        # shipping_address_form = ShippingAddressForm(request.POST)
+        # credit_card_form = CreditCardForm(request.POST)
 
-        if not (purchaser_form.is_valid() and 
-            shipping_address_form.is_valid() and
-            credit_card_form.is_valid()
-            ):
-            # redirect先を考える
-            return
+        # if not (purchaser_form.is_valid() and 
+        #     shipping_address_form.is_valid() and
+        #     credit_card_form.is_valid()
+        #     ):
+        if purchaser_form.is_valid():
+            print(purchaser_form)
+            # バリデーション NGの場合、カートページにリダイレクト
+            return redirect('cart:checkout')
         
-        try:
-            with transaction.atomic():
-                # Puchaserの保存
-                purchaser = purchaser_form.save()
+        # try:
+        #     with transaction.atomic():
+        #         # Puchaserの保存
+        #         purchaser = purchaser_form.save()
                 
-                # ShippingAdressの保存
-                shipping_address = shipping_address_form.save(commit=False)
-                shipping_address.purchaser = purchaser
-                shipping_address.save()
+        #         # ShippingAdressの保存
+        #         shipping_address = shipping_address_form.save(commit=False)
+        #         shipping_address.purchaser = purchaser
+        #         shipping_address.save()
                 
-                # CreditCardの保存
-                credit_card = credit_card_form.save(commit=False)
-                credit_card.purchaser = purchaser
-                credit_card =credit_card_form.save()
-            return 
+        #         # CreditCardの保存
+        #         credit_card = credit_card_form.save(commit=False)
+        #         credit_card.purchaser = purchaser
+        #         credit_card =credit_card_form.save()
+        #     return 
         
-        except Exception as err:
-            # Print文の内容をFlashメッセージで表示させる
-            print(f'予期せぬエラーが発生しました: {err}')
+        # except Exception as err:
+        #     # Print文の内容をFlashメッセージで表示させる
+        #     print(f'予期せぬエラーが発生しました: {err}')
 
 
