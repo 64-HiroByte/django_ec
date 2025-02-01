1431ff29c70b2623f6a3b301f62e977b4cda73f0
64-HiroByte <higher.ground.rc@gmail.com>
2025-02-01 16:44:17 +0900

    PromotionCodeモデルにクラス変数を設定した。promotion_code_generate.pyにバリデータを設定した。

--- promotion/management/commands/promotion_code_generate.py
+++ promotion/management/commands/promotion_code_generate.py
@@ -1,9 +1,11 @@
 import random
 import string
+import sys
 
 from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
 
-# from promotion.models import PromotionCode
+from promotion.models import PromotionCode
 
 
 class Command(BaseCommand):
@@ -16,16 +18,66 @@ class Command(BaseCommand):
         parser.add_argument('-c', '--count', default=7, type=int, help='number of characters in promotion code')
         parser.add_argument('-g', '--generate', default=10, type=int, help='number of promotion codes to generate')
     
-    def handle(self, *args, **options):
+    def arguments_validator(self, options):
+        """
+        オプションを入力した場合のバリデーションを行う
+
+        Args:
+            options (dict): カスタムコマンドで使用する引数と値の辞書
+
+        Raises:
+            CommandError: 不正な値が入力された場合に発生する
+
+        Returns:
+            tuple: バリデーションを通過した値のタプル
+        """
         lower_limit = options.get('lower')
         upper_limit = options.get('upper')
         increment = options.get('increment')
         code_length = options.get('count')
         generate_num = options.get('generate')
         
+        if lower_limit < PromotionCode.MIN_DISCOUNT_AMOUNT:
+            raise CommandError(
+                f'割引額の下限は、{PromotionCode.MIN_DISCOUNT_AMOUNT}以上の整数である必要があります'
+            )
+        if upper_limit > PromotionCode.MAX_DISCOUNT_AMOUNT:
+            raise CommandError(
+                f'割引額の上限は、{PromotionCode.MAX_DISCOUNT_AMOUNT}以下の整数である必要があります'
+            )
+            
+        if lower_limit > upper_limit :
+            raise CommandError(
+                f'割引額の上限値({upper_limit})は、下限値({lower_limit})よりも大きい値である必要があります'
+            )
+        
+        if increment < 1:
+            raise CommandError(
+                f'割引額の増分は、1以上の整数である必要があります'
+            )
+            
+        if code_length < PromotionCode.MIN_CODE_LENGTH or PromotionCode.MAX_CODE_LENGTH < code_length :
+            raise CommandError(
+                f'生成するプロモーションコードの文字数は、{PromotionCode.MIN_CODE_LENGTH}文字以上、{PromotionCode.MAX_CODE_LENGTH}文字以下である必要があります'
+            )
+            
+        if generate_num < 1:
+            raise CommandError(
+                f'生成するコードは、1以上の整数である必要があります'
+            )
+        
+        return lower_limit, upper_limit, increment, code_length, generate_num
+    
+    def handle(self, *args, **options):
+        try:
+            lower_limit, upper_limit, increment, code_length, generate_num = self.arguments_validator(options)
+        except CommandError as e:
+            self.stderr.write(self.style.ERROR(e))
+            sys.exit(1)
+        
         for _ in range(generate_num):
             chars = string.ascii_letters + string.digits
-            generated_code = ''.join([random.choice(chars) for _ in range(code_length)])
+            generated_code = ''.join(random.choices(chars, k=code_length))
             discount_amount = random.randrange(lower_limit, upper_limit + 1, increment)
             print(f'code: {generated_code}, discount amount: {discount_amount}')
         
--- promotion/models.py
+++ promotion/models.py
@@ -12,12 +12,22 @@ class PromotionCode(models.Model):
         discount_amount(int): 割引額。100〜1,000の間の整数（バリデータ適用）
         is_active(bool): 使用可能であるかを示すフラグ
     """
+    # プロモーションコードの長さの制約
+    MIN_CODE_LENGTH = 4
+    MAX_CODE_LENGTH = 32
+
+    # 割引額の制約
+    MIN_DISCOUNT_AMOUNT = 10
+    MAX_DISCOUNT_AMOUNT = 10000
     code = models.CharField(
-        verbose_name='プロモーションコード', max_length=7, unique=True
+        verbose_name='プロモーションコード', max_length=MAX_CODE_LENGTH, unique=True
     )
     discount_amount = models.IntegerField(
         verbose_name='割引額', 
-        validators=[MinValueValidator(100), MaxValueValidator(1000)]
+        validators=[
+            MinValueValidator(MIN_DISCOUNT_AMOUNT), 
+            MaxValueValidator(MAX_DISCOUNT_AMOUNT)
+        ]
     )
     is_active = models.BooleanField(verbose_name='使用可能', default=True)
     created_at = models.DateTimeField(verbose_name='作成日', auto_now_add=True)
